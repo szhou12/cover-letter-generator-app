46208cecdd515c97c1212f4873d5d1dabb626aa5
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-03-07 18:20:34 -0600

    made prompt changes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if selected_job_type in ["Others", "Intern"]:
 
 st.header('Company')
 company_name = st.text_input("Name:", placeholder="e.g. Google")
-company_brief = st.text_area("Brief Introduction:", placeholder="a company that ...", height=100)
+company_brief = st.text_area("Brief Introduction:", height=100)
 
 st.header('Job Requirements')
 # Add a text area for users to type in job requirements
@@ -76,25 +76,27 @@ if selected_job_type and company_name:
 
 
 ## Role prompt
-prompt1 = "Please act as an experienced hiring manager and help me improve my cover letter based on the company and job requirements."
+prompt_role = "Please act as an experienced hiring manager and help me improve my cover letter based on the company and job requirements."
 
 ## Company and job info prompt
-prompt2 = f"I am applying for the [JOB POSITION] at {company_name}, a company that {company_brief}"
+prompt_basic_info = f"I am applying for the [JOB POSITION] at {company_name}."
 if selected_job_type in ["Others", "Intern"]:
-    prompt2 = prompt2.replace("[JOB POSITION]", other_job_type)
+    prompt_basic_info = prompt_basic_info.replace("[JOB POSITION]", other_job_type)
 else:
-    prompt2 = prompt2.replace("[JOB POSITION]", selected_job_type)
+    prompt_basic_info = prompt_basic_info.replace("[JOB POSITION]", selected_job_type)
+
+prompt_company_info = f"The brief introduction of this company goes as: {company_brief}"
 
 ## Job responsibility prompt
-prompt3 = f"The job responsibilities listed as: {job_responsibility}"
+prompt_res = f"The job responsibilities are described as: {job_responsibility}"
 
 ## Job requirements prompt
-prompt4 = f"The job requirements listed as: {job_requirement}"
+prompt_req = f"The job requirements are described as: {job_requirement}"
 
 ## Experience highlight prompt
-prompt5 = f"Based on the given information, please help me customize my cover letter provided below by highlighting my relevant experience in {selected_highlight_type}: \n{cover_letter_template}"
+prompt_highlight = f"Based on the given information, please help me customize my cover letter provided below by highlighting my relevant experience in {selected_highlight_type}: \n{cover_letter_template}"
 
-prompts = [prompt1, prompt2, prompt3, prompt4, prompt5]
+prompts = [prompt_role, prompt_basic_info, prompt_company_info, prompt_res, prompt_req, prompt_highlight]
 prompts = "\n".join(prompts)
 
 # if selected_job_type and company_name and company_brief and job_responsibility and job_requirement and selected_highlight_type:
